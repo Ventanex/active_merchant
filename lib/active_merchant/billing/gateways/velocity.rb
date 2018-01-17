9b0e1cdabf410fb4e95d92206ec1dec4f5231795
Brendon <bharris@ventanex.com>
2018-01-17 16:39:57 -0600

    parsing responses

--- lib/active_merchant/billing/gateways/velocity.rb
+++ lib/active_merchant/billing/gateways/velocity.rb
@@ -1,25 +1,3 @@
-# require 'active_merchant'
-#
-# gateway = ActiveMerchant::Billing::VelocityGateway.new({
-#   identity_token: 'PHNhbWw6QXNzZXJ0aW9uIE1ham9yVmVyc2lvbj0iMSIgTWlub3JWZXJzaW9uPSIxIiBBc3NlcnRpb25JRD0iXzU4ZTM5MGFjLTE0OWItNDNiYS1iOGU0LTQ3ZmEzOWUyZWM3MCIgSXNzdWVyPSJJcGNBdXRoZW50aWNhdGlvbiIgSXNzdWVJbnN0YW50PSIyMDE2LTA5LTIwVDIxOjQ5OjE1LjI0MloiIHhtbG5zOnNhbWw9InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjEuMDphc3NlcnRpb24iPjxzYW1sOkNvbmRpdGlvbnMgTm90QmVmb3JlPSIyMDE2LTA5LTIwVDIxOjQ5OjE1LjI0MloiIE5vdE9uT3JBZnRlcj0iMjA0Ni0wOS0yMFQyMTo0OToxNS4yNDJaIj48L3NhbWw6Q29uZGl0aW9ucz48c2FtbDpBZHZpY2U+PC9zYW1sOkFkdmljZT48c2FtbDpBdHRyaWJ1dGVTdGF0ZW1lbnQ+PHNhbWw6U3ViamVjdD48c2FtbDpOYW1lSWRlbnRpZmllcj4xMzEzODY4NTQxMzAwMDAxPC9zYW1sOk5hbWVJZGVudGlmaWVyPjwvc2FtbDpTdWJqZWN0PjxzYW1sOkF0dHJpYnV0ZSBBdHRyaWJ1dGVOYW1lPSJTQUsiIEF0dHJpYnV0ZU5hbWVzcGFjZT0iaHR0cDovL3NjaGVtYXMuaXBjb21tZXJjZS5jb20vSWRlbnRpdHkiPjxzYW1sOkF0dHJpYnV0ZVZhbHVlPjEzMTM4Njg1NDEzMDAwMDE8L3NhbWw6QXR0cmlidXRlVmFsdWU+PC9zYW1sOkF0dHJpYnV0ZT48c2FtbDpBdHRyaWJ1dGUgQXR0cmlidXRlTmFtZT0iU2VyaWFsIiBBdHRyaWJ1dGVOYW1lc3BhY2U9Imh0dHA6Ly9zY2hlbWFzLmlwY29tbWVyY2UuY29tL0lkZW50aXR5Ij48c2FtbDpBdHRyaWJ1dGVWYWx1ZT5hNWMyYmQ3ZS1iMTA3LTQ0YjYtODhhZS01YjM2ZTYxYjEyY2U8L3NhbWw6QXR0cmlidXRlVmFsdWU+PC9zYW1sOkF0dHJpYnV0ZT48c2FtbDpBdHRyaWJ1dGUgQXR0cmlidXRlTmFtZT0ibmFtZSIgQXR0cmlidXRlTmFtZXNwYWNlPSJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcyI+PHNhbWw6QXR0cmlidXRlVmFsdWU+MTMxMzg2ODU0MTMwMDAwMTwvc2FtbDpBdHRyaWJ1dGVWYWx1ZT48L3NhbWw6QXR0cmlidXRlPjwvc2FtbDpBdHRyaWJ1dGVTdGF0ZW1lbnQ+PFNpZ25hdHVyZSB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS94bWxkc2lnIyI+PFNpZ25lZEluZm8+PENhbm9uaWNhbGl6YXRpb25NZXRob2QgQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzEwL3htbC1leGMtYzE0biMiPjwvQ2Fub25pY2FsaXphdGlvbk1ldGhvZD48U2lnbmF0dXJlTWV0aG9kIEFsZ29yaXRobT0iaHR0cDovL3d3dy53My5vcmcvMjAwMC8wOS94bWxkc2lnI3JzYS1zaGExIj48L1NpZ25hdHVyZU1ldGhvZD48UmVmZXJlbmNlIFVSST0iI181OGUzOTBhYy0xNDliLTQzYmEtYjhlNC00N2ZhMzllMmVjNzAiPjxUcmFuc2Zvcm1zPjxUcmFuc2Zvcm0gQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwLzA5L3htbGRzaWcjZW52ZWxvcGVkLXNpZ25hdHVyZSI+PC9UcmFuc2Zvcm0+PFRyYW5zZm9ybSBBbGdvcml0aG09Imh0dHA6Ly93d3cudzMub3JnLzIwMDEvMTAveG1sLWV4Yy1jMTRuIyI+PC9UcmFuc2Zvcm0+PC9UcmFuc2Zvcm1zPjxEaWdlc3RNZXRob2QgQWxnb3JpdGhtPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwLzA5L3htbGRzaWcjc2hhMSI+PC9EaWdlc3RNZXRob2Q+PERpZ2VzdFZhbHVlPnRhbllYWlVzQmNibjhrYWhmREdwYTFkelZGUT08L0RpZ2VzdFZhbHVlPjwvUmVmZXJlbmNlPjwvU2lnbmVkSW5mbz48U2lnbmF0dXJlVmFsdWU+SGdsMktLR0VPcVBQUDAydnVTRnRHTTEwMkxoZEI2OXp2ZXh3ejZzN2VqQTBBNUl4NGk0U2FsUE1ZYW85MDJkUjZUSzY0eWxCb3oxRnJ2OXpFZUpHQW9hWjZWbktZSVNENTlmSU03dDJYME5SVk9YM1daRTFaRXhwaG1zL3JJL0EzcW41SDlBT1l5OUxhMXBlVHA2TytBenVWYzFONjdBM214WmpERlIyNGhlallwVHJ2TU9PNHQ3M1c2Tm9nMDlSaTRjclRLNERTN21Mekp2VkRzRlB1bkF1dVN4eVBDK0l4VDVNTXBaSDcxNkdzdXhmU0R1anVXaEk1YkV1bFVrNEQyL2s3MEE3RDdkRjJIV3VEMEtuUXhrV2dKM2pVNldyUHp2UC9Mc3djaFZSOWdqNHd6WnpqTXRpY3N3VXJMYnorVXpJY0h1S0IySkw2djdUZTNYb3JnPT08L1NpZ25hdHVyZVZhbHVlPjxLZXlJbmZvPjxvOlNlY3VyaXR5VG9rZW5SZWZlcmVuY2UgeG1sbnM6bz0iaHR0cDovL2RvY3Mub2FzaXMtb3Blbi5vcmcvd3NzLzIwMDQvMDEvb2FzaXMtMjAwNDAxLXdzcy13c3NlY3VyaXR5LXNlY2V4dC0xLjAueHNkIj48bzpLZXlJZGVudGlmaWVyIFZhbHVlVHlwZT0iaHR0cDovL2RvY3Mub2FzaXMtb3Blbi5vcmcvd3NzL29hc2lzLXdzcy1zb2FwLW1lc3NhZ2Utc2VjdXJpdHktMS4xI1RodW1icHJpbnRTSEExIj5ZREJlRFNGM0Z4R2dmd3pSLzBwck11OTZoQ2M9PC9vOktleUlkZW50aWZpZXI+PC9vOlNlY3VyaXR5VG9rZW5SZWZlcmVuY2U+PC9LZXlJbmZvPjwvU2lnbmF0dXJlPjwvc2FtbDpBc3NlcnRpb24+',
-#   work_flow_id: '8D9DE00001',
-#   application_profile_id: 71228,
-#   merchant_profile_id: 'EPX Retail Test'
-# })
-#
-# amount = 1000  # $10.00
-#
-# credit_card = ActiveMerchant::Billing::CreditCard.new(
-#                 :first_name         => 'Bob',
-#                 :last_name          => 'Bobsen',
-#                 :number             => '4242424242424242',
-#                 :month              => '8',
-#                 :year               => Time.now.year+1,
-#                 :verification_value => '000')
-#
-# gateway.purchase(amount, credit_card)
-
-require 'byebug'
 require 'nokogiri'
 
 module ActiveMerchant #:nodoc:
@@ -35,7 +13,16 @@ module ActiveMerchant #:nodoc:
       self.homepage_url = 'https://nabvelocity.com/'
       self.display_name = 'NAB Velocity'
 
-      STANDARD_ERROR_CODE_MAPPING = {}
+      STANDARD_ERROR_CODE_MAPPING = {
+        '108' => STANDARD_ERROR_CODE[:invalid_number],
+        'EB' => STANDARD_ERROR_CODE[:incorrect_number],
+      }
+
+      APPROVED, DECLINED, ERROR, FRAUD_REVIEW = '00', 1, 13, 3
+
+      CARD_CODE_ERRORS = %w(N S)
+      AVS_ERRORS = %w(A E I N R W Z)
+      AVS_REASON_CODES = %w(27 45)
 
       def initialize(options={})
         requires!(options, :identity_token, :work_flow_id, :application_profile_id, :merchant_profile_id)
@@ -193,38 +180,95 @@ module ActiveMerchant #:nodoc:
       end
 
       def parse(action, body)
-        Hash.from_xml(body)['BankcardTransactionResponsePro'].deep_transform_keys { |k| k.to_s.underscore.to_sym }.except(:xmlns, :"xmlns:i", :addendum)
+        doc = Nokogiri::XML(body)
+        doc.remove_namespaces!
+
+        response = {action: action}
+
+        response[:status] = if(element = doc.at_xpath("//BankcardTransactionResponsePro/Status"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:status_code] = if(element = doc.at_xpath("//BankcardTransactionResponsePro/StatusCode"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:status_message] = if(element = doc.at_xpath("//BankcardTransactionResponsePro/StatusMessage"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:avs_result_code] = if(element = doc.at_xpath("//AVSResult//ActualResult"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:transaction_id] = if(element = doc.at_xpath("//ServiceTransactionId"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:transaction_state] = if(element = doc.at_xpath("//TransactionState"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:card_code] = if(element = doc.at_xpath("//CVResult"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:cardholder_authentication_code] = if(element = doc.at_xpath("//PaymentAccountDataToken"))
+          empty?(element.content) ? nil : element.content
+        end
+
+        response[:account_number] = if(element = doc.at_xpath("//MaskedPAN"))
+          empty?(element.content) ? nil : element.content[-4..-1]
+        end
+
+        response
       end
 
       def commit(action, &payload)
-        request = post_data(action, &payload)
-        raw_response = ssl_post(live_url + "/Txn/#{@work_flow_id}", request, headers)
+        raw_response = ssl_post(live_url + "/Txn/#{@work_flow_id}", post_data(action, &payload), headers)
         response = parse(action, raw_response)
 
+        avs_result = AVSResult.new(code: response[:avs_result_code])
+        cvv_result = CVVResult.new(response[:card_code])
         Response.new(
           success_from(response),
-          message_from(response),
+          message_from(response, avs_result, cvv_result),
           response,
-          authorization: authorization_from(response),
-          avs_result: AVSResult.new(response["avs_result"]),
-          cvv_result: CVVResult.new(response["cvv_result"]),
+          authorization: authorization_from(action, response),
           test: test?,
-          error_code: error_code_from(response)
+          avs_result: avs_result,
+          cvv_result: cvv_result,
+          fraud_review: fraud_review?(response),
+          error_code: map_error_code(response[:status_code])
         )
       end
 
       def success_from(response)
+        [APPROVED, FRAUD_REVIEW].include?(response[:status_code])
       end
 
-      def message_from(response)
-      end
-
-      def authorization_from(response)
-      end
-
-      def error_code_from(response)
-        unless success_from(response)
+      def message_from(response, avs_result, cvv_result)
+        if response[:transaction_state] == 'CaptureDeclined'
+          if CARD_CODE_ERRORS.include?(cvv_result.code)
+            return cvv_result.message
+          elsif(AVS_REASON_CODES.include?(response[:status_code]) && AVS_ERRORS.include?(avs_result.code))
+            return avs_result.message
+          end
         end
+
+        response[:status_message]
+      end
+
+      def authorization_from(action, response)
+        [response[:transaction_id], response[:account_number], action].join("#")
+      end
+
+      def fraud_review?(response)
+        (response[:status_code] == FRAUD_REVIEW)
+      end
+
+      def map_error_code(status_code)
+        STANDARD_ERROR_CODE_MAPPING["#{status_code}"]
       end
 
       def post_data(action)
@@ -263,76 +307,76 @@ module ActiveMerchant #:nodoc:
         }
       end
 
-      def authorize_xml(params)
-        Nokogiri::XML::Builder.new do |xml|
-          xml.AuthorizeTransaction('xmlns:i' => 'http://www.w3.org/2001/XMLSchema-instance', 'xmlns' => 'http://schemas.ipcommerce.com/CWS/v2.0/Transactions/Rest', 'i:type' =>"AuthorizeTransaction" ) do
-            xml.ApplicationProfileId @application_profile_id
-            xml.MerchantProfileId @merchant_profile_id
-            xml.Transaction('xmlns:ns1' => "http://schemas.ipcommerce.com/CWS/v2.0/Transactions/Bankcard", 'i:type' => "ns1:BankcardTransaction" ) do
-              xml['ns1'].TenderData do
-                xml['ns1'].CardData do
-                  xml['ns1'].CardType params[:card_type]
-                  xml['ns1'].CardholderName params[:card_holder_name]
-                  if params[:track_2_data].present?
-                    xml['ns1'].Track2Data params[:track_2_data]
-                    xml['ns1'].PAN('i:nil' =>"true")
-                    xml['ns1'].Expire('i:nil' =>"true")
-                    xml['ns1'].Track1Data('i:nil' =>"true")
-                  elsif params[:track_1_data].present?
-                    xml['ns1'].Track1Data params[:track_1_data]
-                    xml['ns1'].PAN('i:nil' =>"true")
-                    xml['ns1'].Expire('i:nil' =>"true")
-                    xml['ns1'].Track2Data('i:nil' =>"true")
-                  else
-                    xml['ns1'].PAN params[:pan]
-                    xml['ns1'].Expire params[:expire]
-                    xml['ns1'].Track1Data('i:nil' =>"true")
-                    xml['ns1'].Track2Data('i:nil' =>"true")
-                  end
-                end
-                xml['ns1'].CardSecurityData do
-                  xml['ns1'].AVSData do
-                    xml['ns1'].CardholderName('i:nil' =>"true")
-                    xml['ns1'].Street params[:Street]
-                    xml['ns1'].City params[:city]
-                    xml['ns1'].StateProvince params[:state_province]
-                    xml['ns1'].PostalCode params[:postal_code]
-                    xml['ns1'].Phone params[:phone]
-                    xml['ns1'].Email params[:email]
-                  end
-                  xml['ns1'].CVDataProvided 'Provided'
-                  xml['ns1'].CVData params[:cv_data]
-                  xml['ns1'].KeySerialNumber('i:nil' =>"true")
-                  xml['ns1'].PIN('i:nil' =>"true")
-                  xml['ns1'].IdentificationInformation('i:nil' =>"true")
-                end
-                xml['ns1'].EcommerceSecurityData('i:nil' =>"true")
-              end
-              xml['ns1'].TransactionData do
-                if params[:amount] != ''
-                  xml['ns8'].Amount('xmlns:ns8' =>"http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text(params[:amount])
-                else
-                  xml['ns8'].Amount('xmlns:ns8' =>"http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text('0.00')
-                end
-                xml['ns9'].CurrencyCode('xmlns:ns9' =>"http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text('USD')
-                xml['ns10'].TransactionDateTime('xmlns:ns10' => "http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text('2014-04-03T13:50:16')
-                xml['ns1'].AccountType 'NotSet'
-                xml['ns1'].CustomerPresent 'Present'
-                xml['ns1'].EmployeeId '11'
-                if params[:track_2_data].present? || params[:track_1_data].present?
-                  xml['ns1'].EntryMode params[:entry_mode]
-                else
-                  xml['ns1'].EntryMode 'Keyed'
-                end
-                xml['ns1'].IndustryType params[:industry_type]
-                xml['ns1'].InvoiceNumber('i:nil' =>"true")
-                xml['ns1'].OrderNumber('i:nil' =>"true")
-                xml['ns1'].TipAmount '0.0'
-              end
-            end
-          end
-        end
-      end
+      # def authorize_xml(params)
+      #   Nokogiri::XML::Builder.new do |xml|
+      #     xml.AuthorizeTransaction('xmlns:i' => 'http://www.w3.org/2001/XMLSchema-instance', 'xmlns' => 'http://schemas.ipcommerce.com/CWS/v2.0/Transactions/Rest', 'i:type' =>"AuthorizeTransaction" ) do
+      #       xml.ApplicationProfileId @application_profile_id
+      #       xml.MerchantProfileId @merchant_profile_id
+      #       xml.Transaction('xmlns:ns1' => "http://schemas.ipcommerce.com/CWS/v2.0/Transactions/Bankcard", 'i:type' => "ns1:BankcardTransaction" ) do
+      #         xml['ns1'].TenderData do
+      #           xml['ns1'].CardData do
+      #             xml['ns1'].CardType params[:card_type]
+      #             xml['ns1'].CardholderName params[:card_holder_name]
+      #             if params[:track_2_data].present?
+      #               xml['ns1'].Track2Data params[:track_2_data]
+      #               xml['ns1'].PAN('i:nil' =>"true")
+      #               xml['ns1'].Expire('i:nil' =>"true")
+      #               xml['ns1'].Track1Data('i:nil' =>"true")
+      #             elsif params[:track_1_data].present?
+      #               xml['ns1'].Track1Data params[:track_1_data]
+      #               xml['ns1'].PAN('i:nil' =>"true")
+      #               xml['ns1'].Expire('i:nil' =>"true")
+      #               xml['ns1'].Track2Data('i:nil' =>"true")
+      #             else
+      #               xml['ns1'].PAN params[:pan]
+      #               xml['ns1'].Expire params[:expire]
+      #               xml['ns1'].Track1Data('i:nil' =>"true")
+      #               xml['ns1'].Track2Data('i:nil' =>"true")
+      #             end
+      #           end
+      #           xml['ns1'].CardSecurityData do
+      #             xml['ns1'].AVSData do
+      #               xml['ns1'].CardholderName('i:nil' =>"true")
+      #               xml['ns1'].Street params[:Street]
+      #               xml['ns1'].City params[:city]
+      #               xml['ns1'].StateProvince params[:state_province]
+      #               xml['ns1'].PostalCode params[:postal_code]
+      #               xml['ns1'].Phone params[:phone]
+      #               xml['ns1'].Email params[:email]
+      #             end
+      #             xml['ns1'].CVDataProvided 'Provided'
+      #             xml['ns1'].CVData params[:cv_data]
+      #             xml['ns1'].KeySerialNumber('i:nil' =>"true")
+      #             xml['ns1'].PIN('i:nil' =>"true")
+      #             xml['ns1'].IdentificationInformation('i:nil' =>"true")
+      #           end
+      #           xml['ns1'].EcommerceSecurityData('i:nil' =>"true")
+      #         end
+      #         xml['ns1'].TransactionData do
+      #           if params[:amount] != ''
+      #             xml['ns8'].Amount('xmlns:ns8' =>"http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text(params[:amount])
+      #           else
+      #             xml['ns8'].Amount('xmlns:ns8' =>"http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text('0.00')
+      #           end
+      #           xml['ns9'].CurrencyCode('xmlns:ns9' =>"http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text('USD')
+      #           xml['ns10'].TransactionDateTime('xmlns:ns10' => "http://schemas.ipcommerce.com/CWS/v2.0/Transactions").text('2014-04-03T13:50:16')
+      #           xml['ns1'].AccountType 'NotSet'
+      #           xml['ns1'].CustomerPresent 'Present'
+      #           xml['ns1'].EmployeeId '11'
+      #           if params[:track_2_data].present? || params[:track_1_data].present?
+      #             xml['ns1'].EntryMode params[:entry_mode]
+      #           else
+      #             xml['ns1'].EntryMode 'Keyed'
+      #           end
+      #           xml['ns1'].IndustryType params[:industry_type]
+      #           xml['ns1'].InvoiceNumber('i:nil' =>"true")
+      #           xml['ns1'].OrderNumber('i:nil' =>"true")
+      #           xml['ns1'].TipAmount '0.0'
+      #         end
+      #       end
+      #     end
+      #   end
+      # end
     end
   end
 end
