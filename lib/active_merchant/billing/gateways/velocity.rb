158278c816a45947ebf90d371d2fa51aed30b00f
William Murphy <wmurphy@ventanex.com>
2019-02-22 18:12:55 -0600

    no message

--- lib/active_merchant/billing/gateways/velocity.rb
+++ lib/active_merchant/billing/gateways/velocity.rb
@@ -505,7 +505,9 @@ module ActiveMerchant #:nodoc:
 
       def adjust(action, &payload)
         begin
-          raw_response = ssl_put(repayment_url + "/CWS/1.1/REST/TPS.svc/#{@work_flow_id}/#{@transaction_id}", reverse_data(action, &payload), headers)
+          puts "url: #{repayment_url + "/CWS/1.1/REST/TPS.svc/#{@work_flow_id}/#{@transaction_id}"}"
+          puts "data: #{reverse_data(action, &payload)}"
+          raw_response = ssl_put(repayment_url + "/CWS/1.1/REST/TPS.svc/#{@work_flow_id}/#{@transaction_id}", reverse_data(action, &payload), json_headers)
           puts "raw_response: #{raw_response}"
           response = parse_response(action, raw_response)
 
@@ -697,7 +699,7 @@ module ActiveMerchant #:nodoc:
           'Content-Type' => 'application/json',
           'Authorization' => "Basic #{Base64.strict_encode64(@identity_token.gsub(/"/, '').concat(":"))}"
         })
-
+        puts "token: #{Base64.strict_encode64(token.gsub(/"/, '').concat(":"))}"
         {
           'Content-Type' => 'application/json',
           'Authorization' => "Basic #{Base64.strict_encode64(token.gsub(/"/, '').concat(":"))}"
